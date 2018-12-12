cf26578263f20c0cb88f2ff02057e1ff86ad8bfd
SherryXiaoyingLi <xl2gs@virginia.com>
2018-12-12 14:42:21 -0500

    Similarity measures for textrank and a small evaluator

--- src/sum/TextRankSummary.java
+++ src/sum/TextRankSummary.java
@@ -1,6 +1,10 @@
 package sum;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import sum.Graph;
 import sum.Sentence;
@@ -11,11 +15,19 @@ public class TextRankSummary extends TextRank implements SummaryExtractor {
     private final Sentence[] sentences;
     private final Graph<Sentence> sentenceGraph;
     private final int summaryLength;
+    private HashMap<String, Float> bm25stats;
 
     public TextRankSummary(Sentence[] s, int l) {
         sentences = s;
         sentenceGraph = new Graph<Sentence>();
         summaryLength = l;
+        bm25stats = new HashMap<>();
+        bm25stats.put("N", (float)s.length);
+        float sum = 0;
+        for (Sentence sen: s) {
+        sum +=sen.getWords().size();
+        }
+        bm25stats.put("navg", sum/s.length);
     }
 
     @Override
@@ -36,14 +48,175 @@ public class TextRankSummary extends TextRank implements SummaryExtractor {
             super(sentenceGraph, s);
         }
 
+        // similarity measure calculated here
         @Override
         public double calculateRelationScore(Graph<Sentence>.Node other) {
-            List<String> thisWords = getContent().getWords();
-            List<String> otherWords = other.getContent().getWords();
-
-            long numCommonWords = thisWords.stream().filter(w -> otherWords.contains(w)).count();
-
-            return numCommonWords / Math.log(thisWords.size() * otherWords.size());
+        		//return BM25(other);
+        		//return TFIDF(other);
+        		return PivotedLength(other);
+        		
+        		//List<String> thisWords = getContent().getWords();
+            //List<String> otherWords = other.getContent().getWords();
+        		//float numCommonWords = thisWords.stream().filter(w -> otherWords.contains(w)).count();
+            //return numCommonWords / Math.log(thisWords.size() * otherWords.size());
         }
+        
+        public double BM25(Graph<Sentence>.Node other) {
+    		//BM25
+    		Set<String> thisWords = new HashSet<>();
+    		Map<String, Integer> thisWordsMap = new HashMap<>();
+    		Map<String, Integer> otherWords = new HashMap<>();
+    		thisWords.addAll(getContent().getWords());
+    		
+    		for (String ow:other.getContent().getWords()) {
+    			int cnt = 1;
+    			if (otherWords.containsKey(ow)) {
+    				otherWords.put(ow, otherWords.get(ow)+cnt);
+    			}
+    			else {
+    				otherWords.put(ow, cnt);
+    			}
+    		}
+    		for (String ow:getContent().getWords()) {
+    			int cnt = 1;
+    			if (thisWordsMap.containsKey(ow)) {
+    				thisWordsMap.put(ow, thisWordsMap.get(ow)+cnt);
+    			}
+    			else {
+    				thisWordsMap.put(ow, cnt);
+    			}
+    		}
+    		float N = bm25stats.get("N");
+    		float docLength = other.getContent().getWords().size();
+    		// this(q) other(d)
+    		float df = 0;
+    		for (Sentence sen: sentences) {
+    			boolean check = true;
+    			for (String w: thisWords) {
+    				if (!sen.getWords().contains(w)) {
+    					check = false;
+    					break;
+    				}
+    			}
+    			if (check) {
+    				df ++;
+    			}
+    		}
+    		
+    		float navg = bm25stats.get("navg"); 
+    		float k1 = (float) 1.2;  
+    		float k2 = 750;
+    		float b = (float) 0.75;
+    		float score = 0;
+    		for (String w: thisWords) {
+    			if (otherWords.containsKey(w)) {
+    				float termFreq = otherWords.get(w);
+    				float queryTermFreq = thisWordsMap.get(w);
+    				score += (float) (Math.log( (N - df  + 0.5) / (df + 0.5) ) 
+    	    				* ( (k1+1) * termFreq) / (k1 * (1-b+b*docLength/navg) + termFreq) ) 
+    	    				* ( (k2+1)*queryTermFreq/ (k2+queryTermFreq) ); 
+    			}
+    		}
+    		//System.out.println(score);
+    		return score;
+        }
+        
+        public double TFIDF(Graph<Sentence>.Node other) {
+    		Set<String> thisWords = new HashSet<>();
+    		Map<String, Integer> otherWords = new HashMap<>();
+    		thisWords.addAll(getContent().getWords());
+    		
+    		for (String ow:other.getContent().getWords()) {
+    			int cnt = 1;
+    			if (otherWords.containsKey(ow)) {
+    				otherWords.put(ow, otherWords.get(ow)+cnt);
+    			}
+    			else {
+    				otherWords.put(ow, cnt);
+    			}
+    		}
+    		float N = bm25stats.get("N");
+    		float docLength = other.getContent().getWords().size();
+    		// this(q) other(d)
+    		float df = 0;
+    		for (Sentence sen: sentences) {
+    			boolean check = true;
+    			for (String w: thisWords) {
+    				if (!sen.getWords().contains(w)) {
+    					check = false;
+    					break;
+    				}
+    			}
+    			if (check) {
+    				df ++;
+    			}
+    		}
+    		float navg = bm25stats.get("navg"); 
+    		float score = 0;
+    		for (String w: thisWords) {
+    			if (otherWords.containsKey(w)) {
+    				float termFreq = otherWords.get(w);
+    				score += (float) ( (1 + Math.log(termFreq) ) * Math.log( (N + 1) / df ) );
+    			}
+    		}
+    		//System.out.println(score);
+    		return score;
+        }
+        
+        public double PivotedLength(Graph<Sentence>.Node other) {Set<String> thisWords = new HashSet<>();
+        float s = (float) 0.75;
+        Map<String, Integer> thisWordsMap = new HashMap<>();
+    		Map<String, Integer> otherWords = new HashMap<>();
+    		thisWords.addAll(getContent().getWords());
+    		
+    		for (String ow:other.getContent().getWords()) {
+    			int cnt = 1;
+    			if (otherWords.containsKey(ow)) {
+    				otherWords.put(ow, otherWords.get(ow)+cnt);
+    			}
+    			else {
+    				otherWords.put(ow, cnt);
+    			}
+    		}
+    		for (String ow:getContent().getWords()) {
+    			int cnt = 1;
+    			if (thisWordsMap.containsKey(ow)) {
+    				thisWordsMap.put(ow, thisWordsMap.get(ow)+cnt);
+    			}
+    			else {
+    				thisWordsMap.put(ow, cnt);
+    			}
+    		}
+    		float N = bm25stats.get("N");
+    		float docLength = other.getContent().getWords().size();
+    		// this(q) other(d)
+    		float df = 0;
+    		for (Sentence sen: sentences) {
+    			boolean check = true;
+    			for (String w: thisWords) {
+    				if (!sen.getWords().contains(w)) {
+    					check = false;
+    					break;
+    				}
+    			}
+    			if (check) {
+    				df ++;
+    			}
+    		}
+    		float navg = bm25stats.get("navg");
+    		float score = 0;
+    		for (String w: thisWords) {
+    			if (otherWords.containsKey(w)) {
+    				float termFreq = otherWords.get(w);
+    				float queryTermFreq = thisWordsMap.get(w);
+    				score += (float) ((1+Math.log(1+Math.log(termFreq) ) ) / (1-s+s*docLength/navg) 
+    	    				* queryTermFreq * Math.log( (N+1) / df));
+    			}
+    		}
+    		//System.out.println(score);
+    		return score;
+        }
+       
+        
     }
 }
--- src/sum/main.java
+++ src/sum/main.java
@@ -10,9 +10,13 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.DoubleField;
@@ -38,12 +42,13 @@ public class main {
     private static String[] keywords;
 
     private static int numKeywords = 3;
-    private static int summaryLength = 2;
+    private static int summaryLength = 1;
 
     private enum Algorithm {
 	TEXTRANK
     }
-
+    
+    
     public static void main(String[] args) throws IOException {
     	HashMap<String, JSONObject> jsons = LoadDirectory("data/Reviews", ".json");
     	
@@ -67,12 +72,13 @@ public class main {
 						
 						Path path = Paths.get(filename);
 						Files.delete(path);
+						
 					} catch (JSONException e) {
 						e.printStackTrace();
 					}
 				}
 				main_writer.close();
-				BufferedWriter sum_writer = new BufferedWriter(new FileWriter(name+".productSum") );
+				BufferedWriter sum_writer = new BufferedWriter(new FileWriter("data/Reviews/sum/"+productID+".productSum") );
 				getSummary(name+".sum", sum_writer);
 				sum_writer.close();
 				Files.delete(Paths.get(name+".sum"));
@@ -80,11 +86,81 @@ public class main {
 			 	
 	} catch (JSONException e) {
 			e.printStackTrace();
-	}
-    	
+	}	
     }
     
-    public static void getSummary(String filename, BufferedWriter writer) throws IOException {
+    
+    /**
+    //for evaluation here
+    public static void main(String[] args) throws IOException {
+    	HashMap<String, ArrayList<Sentence[]>> sum_gold = new HashMap<>();
+    	File dir = new File("data/Reviews/train/summaries-gold");
+    	if (dir.isDirectory()) {
+    		for (File dir2:dir.listFiles()) {
+    			if (dir2.isDirectory()) {
+    				String topicName = dir2.getName();
+    				ArrayList<Sentence[]> gold = new ArrayList<>();
+    				for (File f: dir2.listFiles()) {
+    				 	try {
+    						p = new Parser(f.getAbsolutePath());
+    					} catch (IOException e) {
+    						e.printStackTrace();
+    					}
+    					gold.add(p.getParsedSentences());
+    				}
+    				sum_gold.put(topicName, gold);
+    			}
+    		}
+    	}
+    			double avgrate = 0;
+			for (String topic_name: sum_gold.keySet() ) {
+				BufferedWriter sum_writer = new BufferedWriter(new FileWriter("data/Reviews/train/"+topic_name+".productSum") );
+				Sentence[] summary = getSummary("data/Reviews/train/topics/"+topic_name+".txt.data", sum_writer);
+				sum_writer.close();
+				//compare result to summaries-gold
+				double topicrate = 0;
+				for (Sentence[] parsedSum: sum_gold.get(topic_name)) {
+					double filerate = 0;
+					for (Sentence sum: summary) {
+						double senrate = 0;
+						List<String> thisWords = sum.getWords();
+						for (Sentence gold_sen: parsedSum) {
+							List<String> otherWords = gold_sen.getWords();
+							
+							//HashSet<String> set = new HashSet<>();
+							//set.addAll(otherWords);
+							//double cnt = 0;
+							//for (String w: set) {
+							//	if (thisWords.contains(w)) {
+							//		cnt ++;
+							//	}
+							//}
+							//senrate += cnt / set.size();
+							//2 added here to keep the result within 1 
+							float numCommonWords = thisWords.stream().filter(w -> otherWords.contains(w)).count();
+							senrate += numCommonWords / 2/Math.log(thisWords.size() * otherWords.size());
+							
+							//senrate += numCommonWords / otherWords.size();
+						}
+						senrate /= parsedSum.length;
+					    filerate += senrate;
+					}
+					filerate /= summary.length;
+					topicrate += filerate;
+				}
+				topicrate /= 5;
+				
+				System.out.println(topicrate);
+				avgrate += topicrate;
+			}
+			avgrate /= sum_gold.size();
+			System.out.println("avgrate: "+avgrate);
+    }
+	
+	**/
+    
+    
+    public static Sentence[] getSummary(String filename, BufferedWriter writer) throws IOException {
     	try {
 			p = new Parser(filename);
 		} catch (IOException e) {
@@ -109,6 +185,7 @@ public class main {
 		for (Sentence s : summary) {
 			writer.write(s.toString()+"\n");
 		}
+		return summary;
     }
     
     
